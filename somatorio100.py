fa262f0cf6dab2db1e7a3217f8fbaded46aada8e
Vinicius Ares <viniciusares321@gmail.com>
2020-04-19 16:27:56 -0300

    first commit from local

---
+++ somatorio100.py
@@ -0,0 +1,12 @@
+# pra nao deixar morrer
+from viniciusscript import Livro
+
+somatorio = 0
+
+for valordavez in range(100):
+	somatorio += valordavez+1
+
+print(somatorio)
+
+for imprime in range(5):
+	print(imprime)
